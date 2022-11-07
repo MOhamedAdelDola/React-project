fde90842c3dcfaee3a5f93e65fa9dbef326e64fb
Mohamed Adel Elsayed Yamany <Mohamedadelyamany921@gmail.com>
2022-11-07 11:10:41 +0200

    First commit to upload the files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState , useEffect } from 'react';
+import PokemonList from './PokemonList';
+import axios from 'axios';
+import Pagination from './pagination';
 
 function App() {
+  const [pokemon,setPokemon] = useState();
+  const [currentPage , setCurrentPage] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPage,setNextPage] = useState();
+  const [prevPage,setPervPage] = useState();
+  const [loading,setLoading] = useState(true);
+
+
+
+
+  useEffect( () =>{
+    setLoading(true)
+    let cancel
+    axios.get(currentPage,{
+      cancelToken : new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPage(res.data.next)
+      setPervPage(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return () => cancel();
+  },[currentPage])
+
+  function goToNextPage(){
+    setCurrentPage(nextPage)
+  }
+  function goToPrevPage(){
+    setCurrentPage(prevPage)
+  }
+  if (loading) return "Loading Please wait ......"
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // because we are returning two components and the return return one item or component
+    // it is an empty html element 
+    <> 
+    <PokemonList pokemon={pokemon} />
+    <Pagination 
+      goToNextPage ={nextPage ? goToNextPage : null}
+      goToPrevPage ={prevPage ? goToPrevPage : null}
+    />
+    </>
   );
 }
 
